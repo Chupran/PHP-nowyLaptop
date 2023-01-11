3a1dc4db01f64bbab7fccc7f63a80783719bad61
Eryk Czuchran <chupran05@gmail.com>
2023-01-11 12:26:33 +0100

    Wymuszona wiadomość

---
+++ Filmoteka/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Filmoteka</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+<header>
+    <div id="up1">
+        <img src="pliki3/klaps.png" alt="Nasze filmy">
+    </div>
+    <div id="up2">
+        <h1>BAZA FILMÓW</h1>
+    </div>
+    <div id="up3">
+        <form method="post">
+            <label>
+                <select name="Filmy">
+                    <option value="Sci-Fi">Sci-Fi</option>
+                    <option value="animacja">animacja</option>
+                    <option value="dramat">dramat</option>
+                    <option value="horror">horror</option>
+                    <option value="komedia">komedia</option>
+                </select>
+            </label>
+            <label>
+                <input type="submit" value="Wyślij">
+            </label>
+        </form>
+    </div>
+    <div id="up4">
+        <img src="pliki3/gwiezdneWojny.jpg" alt="Szturmowcy">
+    </div>
+    <main>
+        <div id="main1">
+            <h2>Wybrano filmy</h2>
+
+        </div>
+        <div id="main2">
+            <h2>Wszystkie filmy</h2>
+        </div>
+    </main>
+</header>
+<footer>
+    <div id="down">
+        <p>Eryk Czuchran</p> <a href="pliki3/dane.sql"> <a href="filmy.pl">
+    </div>
+</footer>
+</body>
+</html>
