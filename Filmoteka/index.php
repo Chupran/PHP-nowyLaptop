296ef47ffd6f984459f926d6d0b8286036d0eb45
Eryk Czuchran <chupran05@gmail.com>
2023-01-24 12:27:50 +0100

    Egzamin zawodowy do zrobienia w domu.

--- Filmoteka/index.php
+++ Filmoteka/index.php
@@ -6,49 +6,41 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Filmoteka</title>
-    <link rel="stylesheet" href="styl3.css">
+    <link rel="stylesheet" href="styl.css">
 </head>
 <body>
 <header>
-    <div id="up1">
+    <section>
         <img src="pliki3/klaps.png" alt="Nasze filmy">
-    </div>
-    <div id="up2">
-        <h1>BAZA FILMÓW</h1>
-    </div>
-    <div id="up3">
+    </section>
+    <section>
+        <h1>BAZA FILMÓW!</h1>
+    </section>
+    <section>
         <form method="post">
             <label>
-                <select name="Filmy">
-                    <option value="Sci-Fi">Sci-Fi</option>
-                    <option value="animacja">animacja</option>
-                    <option value="dramat">dramat</option>
-                    <option value="horror">horror</option>
-                    <option value="komedia">komedia</option>
+                <select>
+                    <option name="Sci-Fi">Sci-Fi</option>
+                    <option name="animacja">animacja</option>
+                    <option name="dramat">dramat</option>
+                    <option name="horror">horror</option>
+                    <option name="komedia">komedia</option>
                 </select>
             </label>
-            <label>
-                <input type="submit" value="Wyślij">
-            </label>
+            <label><input type="submit" value="Wyślij"></label>
         </form>
-    </div>
-    <div id="up4">
+    </section>
+    <section>
         <img src="pliki3/gwiezdneWojny.jpg" alt="Szturmowcy">
-    </div>
-    <main>
-        <div id="main1">
-            <h2>Wybrano filmy</h2>
-
-        </div>
-        <div id="main2">
-            <h2>Wszystkie filmy</h2>
-        </div>
-    </main>
+    </section>
 </header>
-<footer>
-    <div id="down">
-        <p>Eryk Czuchran</p> <a href="pliki3/dane.sql"> <a href="filmy.pl">
-    </div>
-</footer>
+<main>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+</main>
 </body>
 </html>
