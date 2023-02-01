9d7c224b15be3fb9a0d6e1dfb54adef4bf2b7a06
Eryk Czuchran <chupran05@gmail.com>
2023-02-01 09:36:33 +0100

    Egzamin zawodowy do zrobienia w domu.

---
+++ Grzybobranie/Eryk Czuchran/grzyby.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Grzybobranie</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+<header>
+    <section id="miniatury">
+        <a href="pliki5/borowik.jpg"><img src="pliki5/borowik-miniatura.jpg" alt="Idziemy na grzyby!"></a>
+    </section>
+    <section id="tytułowy">
+        <h1>Idziemy na grzyby!</h1>
+    </section>
+</header>
+<main>
+    <section id="lewy">
+
+    </section>
+    <section id="prawy">
+        <h2>Grzyby jadalne</h2>
+
+        <h2>Polecamy do sosów</h2>
+    </section>
+</main>
+<footer>
+    <p>Autor: Eryk Czuchran</p>
+</footer>
+</body>
+</html>
