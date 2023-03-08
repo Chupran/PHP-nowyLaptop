13045df2a289a5c40517e255fd136facc0c8ff51
Eryk Czuchran <chupran05@gmail.com>
2023-03-08 12:27:02 +0100

    Egzamin zawodowy do zrobienia w domu.

---
+++ Hurtownia niebieska/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hurtownia papiernicza</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+<header>
+    <h1>W naszej hurtowni kupisz najtaniej!</h1>
+</header>
+<main>
+    <section id="lewy">
+        <h3>Ceny wybranych artykułów w hurtowni:</h3>
+        <table>
+            <tr>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        </table>
+    </section>
+    <section id="środkowy">
+        <h3>Ile będą kosztować Twoje zakupy?</h3>
+        <form>
+            <label>wybierz artykuł</label>
+        </form>
+    </section>
+    <section id="prawy">
+
+    </section>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
--- PDO/index.php
+++ PDO/index.php
@@ -8,6 +8,25 @@
     <title>Biblioteka PDO</title>
 </head>
 <body>
-
+<form action="index.php" method="post">
+    <label>
+        Podaj imię autora<input type="text" name="nameA">
+    </label>
+    <label>
+        Podaj nazwisko autora<input type="text" name="surnameA">
+    </label>
+    <label>
+        <input type="submit" value="Wyślij dane">
+    </label>
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $name=$_POST['nameA'];
+    $surname=$_POST['surnameA'];
+    $base=new PDO("mysql:host=localhost;dbname=3pir2_biblioteka", "root", "");
+    $base->query("INSERT INTO autorzy (id, imie, nazwisko) VALUES (null, '$name', '$surname');");
+    $base=null;
+    }
+?>
 </body>
 </html>
