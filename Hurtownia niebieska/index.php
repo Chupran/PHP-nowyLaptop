6fbe9ed4b91442656c227dd53e5dc8025ce7250c
Eryk Czuchran <chupran05@gmail.com>
2023-03-15 12:26:04 +0100

    Egzamin zawodowy do zrobienia w domu.

--- Hurtownia niebieska/index.php
+++ Hurtownia niebieska/index.php
@@ -52,16 +52,33 @@
     </section>
     <section id="środkowy">
         <h3>Ile będą kosztować Twoje zakupy?</h3>
-        <form>
-            <label>wybierz artykuł</label>
+        <form action="index.php" method="post">
+            <label>
+                wybierz artykuł
+                <select name="art">
+                    <option value="zeszyt_60_kartek">Zeszyt 60 kartek</option>
+                    <option value="zeszyt_32_kartki">Zeszyt 32 kartki</option>
+                    <option value="cyrkiel">Cyrkiel</option>
+                    <option value="linijka_30_cm">Linijka 30 cm</option>
+                    <option value="ekierka">Ekierka</option>
+                    <option value="linijka_50_cm">Linijka 50 cm</option>
+                </select>
+            </label>
+            <label>Liczba sztuk:<input type="number" name="num"></label>
+            <label><input type="submit" value="OBLICZ"></label>
         </form>
     </section>
     <section id="prawy">
-
+        <img src="materialy2/zakupy2.png" alt="hurtownia">
+        <h3>Kontakt</h3><br>
+        <p>telefon:<br>
+            111222333<br>
+            e-mail:<br>
+            <a href="mailto: hurt@wp.pl">hurt@wp.pl</a> </p>
     </section>
 </main>
 <footer>
-
+    <h4>Witrynę wykonał: Eryk Czuchran</h4>
 </footer>
 </body>
 </html>
