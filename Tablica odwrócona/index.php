69bab886c3885af1d5b5bd53956093553f87e966
Eryk Czuchran <chupran05@gmail.com>
2023-03-28 12:26:31 +0200

    Do dokończenia w domu.

---
+++ Tablica odwrócona/index.php
@@ -0,0 +1,25 @@
+<?php
+for($i=0; $i<4; $i++){
+    for($j=0; $j<4; $j++){
+        $table[$i][$j]=rand(0,1);
+    }
+}
+var_dump($table);
+$result=0;
+$counter=0;
+for($i=0; $i<4; $i++){
+    for($j=0; $j<4; $j++){
+        $counter+=$table[$i][$j];
+    }
+    if($counter>$result){
+        for($i2=0; $i2<4; $i2++){
+            $table2[$i2]=$table[$i][$i2];
+        }
+    }
+}
+echo "<table>
+        <tr>";
+for($i=0; $i<4; $i++){
+    echo"<td>$table2[$i]</td>";
+}
+echo "</tr></table>";
